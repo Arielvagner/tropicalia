78b9e0a0792f32f5e86f86c774dcb0572fe7861a
Arielvagner <arielvagner30@gmail.com>
2025-04-02 21:03:59 -0300

    Add files via upload

---
+++ ariel 3e/tropicalia-main/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade')
+
+botaoDeAcessibilidade.addEventListener('click',funcition(){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresent')
+
+const botaoSelecione=botaoDeAcessibilidade.getAttribute
+
+}
+)
