02097ca59de0741b7dbd1b8c9ec873c7c50e916c
Arielvagner <arielvagner30@gmail.com>
2025-04-03 20:58:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded',function(){
 
     const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
-}
-)
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+    
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+})
+
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+const alternarContraste = document.getElementById('alterna-contraste');
+
+
+
+
+
+})
